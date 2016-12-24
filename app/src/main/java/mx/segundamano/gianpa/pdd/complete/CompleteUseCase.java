95791ca2d56f5441694e8117c051a77f3bbedf8b
Juan Villa <juan64@gmail.com>
2016-12-24 15:06:40 -0600

    Start break from notification

--- app/src/main/java/mx/segundamano/gianpa/pdd/complete/CompleteUseCase.java
+++ app/src/main/java/mx/segundamano/gianpa/pdd/complete/CompleteUseCase.java
@@ -1,20 +1,53 @@
 package mx.segundamano.gianpa.pdd.complete;
 
+import mx.segundamano.gianpa.pdd.alarm.Alarm;
+import mx.segundamano.gianpa.pdd.data.Break;
+import mx.segundamano.gianpa.pdd.data.BreakTimerRepository;
 import mx.segundamano.gianpa.pdd.data.Pomodoro;
 import mx.segundamano.gianpa.pdd.data.PomodoroRepository;
+import mx.segundamano.gianpa.pdd.data.SettingsGateway;
 import mx.segundamano.gianpa.pdd.notify.NotificationGateway;
 
 public class CompleteUseCase {
 
+    private static final String TAG = CompleteUseCase.class.getSimpleName();
+
     private NotificationGateway notificationGateway;
     private PomodoroRepository pomodoroRepository;
+    private SettingsGateway settingsGateway;
+    private BreakTimerRepository breakTimerRepository;
+    private Alarm alarm;
 
-    public CompleteUseCase(PomodoroRepository pomodoroRepository, NotificationGateway notificationGateway) {
+    public CompleteUseCase(PomodoroRepository pomodoroRepository, NotificationGateway notificationGateway, SettingsGateway settingsGateway, BreakTimerRepository breakTimerRepository, Alarm alarm) {
         this.pomodoroRepository = pomodoroRepository;
         this.notificationGateway = notificationGateway;
+        this.settingsGateway = settingsGateway;
+        this.breakTimerRepository = breakTimerRepository;
+        this.alarm = alarm;
     }
 
     public void complete(boolean isComplete) {
+        completePomodoro(isComplete);
+
+        startBreak();
+    }
+
+    public void startBreak() {
+        long startTimeInMillis = System.currentTimeMillis();
+
+        Break activeBreak = new Break();
+        activeBreak.startTimeInMillis = startTimeInMillis;
+        activeBreak.endTimeInMillis = startTimeInMillis + settingsGateway.readBreakTime();
+        activeBreak.status = Break.ACTIVE;
+
+        breakTimerRepository.persist(activeBreak);
+
+        alarm.setWakeUpTime(activeBreak.endTimeInMillis, TAG);
+        alarm.setActiveTimeUpListener(null);
+        notificationGateway.showBreakOnGoingNotification(activeBreak.endTimeInMillis);
+    }
+
+    public void completePomodoro(boolean isComplete) {
         Pomodoro activePomodoro = pomodoroRepository.findTimeUpPomodoro();
         if (activePomodoro != null) {
             activePomodoro.status = isComplete ? Pomodoro.COMPLETE : Pomodoro.DISCARDED;
--- app/src/main/java/mx/segundamano/gianpa/pdd/complete/di/CompleteServiceModule.java
+++ app/src/main/java/mx/segundamano/gianpa/pdd/complete/di/CompleteServiceModule.java
@@ -3,9 +3,12 @@ package mx.segundamano.gianpa.pdd.complete.di;
 import dagger.Module;
 import dagger.Provides;
 import io.realm.Realm;
+import mx.segundamano.gianpa.pdd.alarm.Alarm;
 import mx.segundamano.gianpa.pdd.complete.CompleteService;
 import mx.segundamano.gianpa.pdd.complete.CompleteUseCase;
+import mx.segundamano.gianpa.pdd.data.BreakTimerRepository;
 import mx.segundamano.gianpa.pdd.data.PomodoroRepository;
+import mx.segundamano.gianpa.pdd.data.SettingsGateway;
 import mx.segundamano.gianpa.pdd.notify.NotificationGateway;
 
 @Module
@@ -17,9 +20,9 @@ public class CompleteServiceModule {
     }
 
     @Provides
-    public CompleteUseCase provideCompleteUseCase(NotificationGateway notificationGateway) {
+    public CompleteUseCase provideCompleteUseCase(NotificationGateway notificationGateway, SettingsGateway settingsGateway, BreakTimerRepository breakTimerRepository, Alarm alarm) {
         Realm realm = Realm.getDefaultInstance();
         PomodoroRepository pomodoroRepository = new PomodoroRepository(realm);
-        return new CompleteUseCase(pomodoroRepository, notificationGateway);
+        return new CompleteUseCase(pomodoroRepository, notificationGateway, settingsGateway, breakTimerRepository, alarm);
     }
 }
